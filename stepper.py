a75fabf9c372a15c6896a6a60a33974785bb2a20
Gary Vladimir <gary@garybricks.com>
2024-08-16 16:40:55 -0600

    implemented simple driver for Stepper motor with move_one_step feature

---
+++ stepper.py
@@ -0,0 +1,13 @@
+import machine, utime
+from machine import Pin
+
+
+class Stepper:
+    def __init__(self, step_pin):
+        self.step_pin = Pin(step_pin, mode=Pin.OUT, pull=None)
+
+    def move_one_step(self):
+        self.step_pin.value(True)
+        utime.sleep(0.001)
+        self.step_pin.value(False)
+        utime.sleep(0.001)
