a4c3e7b7cce1fe2a01904a7c518e8ea7538f69e4
Gary Vladimir <gary@garybricks.com>
2024-08-17 12:50:45 -0600

    added left and right wheel stepper motors to rover

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from led import LED
 from button import Button
 from buzzer import Buzzer
+from stepper import Stepper
 from utime import sleep
 
 my_led = LED(12)
 my_btn = Button(27)
 my_buzz = Buzzer(15)
+leftWheel = Stepper(13, 14)
+rightWheel = Stepper(19, 18)
+
 
 my_led.on()
 my_btn.wait_button_press()
 my_led.off()
 my_buzz.beep_once()
+
+for i in range(20):
+    leftWheel.move_one_step(1)
+    rightWheel.move_one_step(1)
 
 """
 while True:
